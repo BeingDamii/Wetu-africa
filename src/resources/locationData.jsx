b064dffe6cec963a5390310fafed974b5c7366a7
Damii dennison <dwanedehydra@gmail.com>
2023-05-31 19:11:12 +0100

    added location data details

--- src/resources/locationData.jsx
+++ src/resources/locationData.jsx
@@ -1,8 +1,35 @@
 import giza from "../resources/giza.png";
-import giza1 from "../resources/giza1.png";
-import giza2 from "../resources/giza2.png";
-import giza3 from "../resources/giza3.png";
-import giza4 from "../resources/giza4.png";
+import giza1 from "../resources/gizaimg1.png";
+import giza2 from "../resources/gizaimg2.png";
+import giza3 from "../resources/gizaimg3.png";
+import giza4 from "../resources/gizaimg4.png";
+
+// second
+import masai from "../resources/masai.png";
+import masaiimg1 from "../resources/masaiimg1.png";
+import masaiimg2 from "../resources/masaiimg2.png";
+import masaiimg3 from "../resources/masaiimg3.png";
+import masaiimg4 from "../resources/masaiimg4.png";
+
+// third
+import victoriafalls from "../resources/victoriafalls.png";
+import victoriafallsimg1 from "../resources/victoriafallsimg1.png";
+import victoriafallsimg2 from "../resources/victoriafallsimg2.png";
+
+// fourth
+import capetown from "../resources/capetown.png";
+import capetownimg1 from "../resources/capetownimg1.png";
+import capetownimg2 from "../resources/capetownimg2.png";
+
+// fifth
+import marrakech from "../resources/marrakech.png";
+import marrakechimg1 from "../resources/marrakechimg1.png";
+import marrakechimg2 from "../resources/marrakechimg2.png";
+
+// sixth
+import serengeti from "../resources/serengeti.png";
+import serengetiimg1 from "../resources/serengetiimg1.png";
+import serengetiimg2 from "../resources/serengetiimg2.png";
 
 const locationData = () => {
   return [
@@ -38,6 +65,133 @@ const locationData = () => {
           img: giza4,
           description:
             "Take a memorable camel ride around the pyramids and experience the desert landscape up close. For the adventurous, consider embarking on a desert excursion, such as a quad biking or dune bashing tour, to further explore the surrounding area and enjoy exhilarating experiences amidst the stunning backdrop of the pyramids.",
+        },
+      ],
+    },
+    {
+      title: "Masai Mara National Reserve, Kenya",
+      shortDescription:
+        "Marvel at the ancient wonders of the world, the iconic pyramids, and the Sphinx in Egypt's vast desert.",
+      longDescription:
+        "Embark on an extraordinary journey to the Maasai Mara National Reserve in Kenya, a haven of untamed wilderness and captivating wildlife. Spread across vast plains and rolling hills, this iconic destination offers an immersive safari experience like no other. As you traverse the golden grasslands, be prepared to witness the awe-inspiring spectacle of the Great Migration, where millions of wildebeest, zebras, and antelopes brave the treacherous Mara River in search of fresh grazing lands. The Maasai Mara is not only renowned for its abundant wildlife but also for its majestic predators, including lions, leopards, and cheetahs, which roam freely in their natural habitat. Immerse yourself in the captivating rhythm of nature, where each dawn unveils breathtaking vistas and each dusk paints the sky in a tapestry of vivid hues. Beyond the enchanting wildlife encounters, the Maasai Mara invites you to discover the vibrant culture of the Maasai people, known for their traditional dress, rich heritage, and welcoming spirit. Whether it's witnessing the thrilling Big Five or embracing the warmth of the Maasai community, the Maasai Mara National Reserve promises an unforgettable adventure, leaving indelible memories etched in your heart.",
+      url: "/location/Masai-Mara",
+      mainImg: masai,
+      sideAttractions: [
+        {
+          title: "Hot Air Balloon Safari",
+          img: masaiimg1,
+          description:
+            "Take to the skies and experience the breathtaking beauty of Masai Mara from a unique perspective. Float above the vast savannah, witnessing the wildlife below and capturing stunning aerial views of the reserve.",
+        },
+        {
+          title: "Maasai Village Visit",
+          img: masaiimg2,
+          description:
+            "Immerse yourself in the vibrant Maasai culture by visiting a traditional Maasai village near the reserve. Learn about their rich traditions, customs, and way of life, and interact with the Maasai people to gain a deeper understanding of their heritage.",
+        },
+        {
+          title: "Maasai Mara Museum",
+          img: masaiimg3,
+          description:
+            "Explore the Maasai Mara Museum, located near the reserve, to delve into the history, culture, and biodiversity of the region. Discover exhibits showcasing traditional Maasai artifacts, wildlife displays, and educational information about the reserve's ecosystem.",
+        },
+        {
+          title: "Nature Walks and Guided Hikes",
+          img: masaiimg4,
+          description:
+            "Embark on guided nature walks or hikes led by experienced local guides to explore the surrounding areas of Masai Mara. Discover hidden gems, encounter unique flora and fauna, and learn about the intricate ecosystems that support the reserve's diverse wildlife.",
+        },
+      ],
+    },
+    {
+      title: "Victoria Falls, Zambia/Zimbabwe",
+      shortDescription:
+        "Stand in awe of the magnificent Victoria Falls, one of the world's largest waterfalls, known as 'The Smoke That Thunders.'",
+      longDescription:
+        "Prepare to be captivated by the breathtaking beauty of Victoria Falls, a natural wonder that straddles the border between Zambia and Zimbabwe. Known as 'The Smoke That Thunders,' this awe-inspiring waterfall cascades with an incredible force as the mighty Zambezi River plummets into the depths of a dramatic gorge. The sheer magnitude of the falls is a sight to behold, with mist rising and rainbows forming amidst the spray. Embark on a thrilling adventure as you explore the surrounding national parks, embark on river safaris, or take a heart-pounding plunge into the Devil's Pool, a natural infinity pool perched on the edge of the falls. Immerse yourself in the raw power of nature and let the grandeur of Victoria Falls leave an indelible impression on your senses.",
+      url: "/location/victoria-falls",
+      mainImg: victoriafalls,
+      sideAttractions: [
+        {
+          title: "Devil's Pool",
+          img: victoriafallsimg1,
+          description:
+            "Experience the thrill of swimming at the edge of Victoria Falls in the Devil's Pool. This natural rock pool provides an exhilarating opportunity to take in the breathtaking views while being safely enclosed by rocks.",
+        },
+        {
+          title: "Zambezi River Sunset Cruise",
+          img: victoriafallsimg2,
+          description:
+            "Embark on a relaxing sunset cruise along the Zambezi River. Enjoy the stunning scenery, spot wildlife along the banks, and witness the magnificent African sunset as it paints the sky in vibrant hues.",
+        },
+      ],
+    },
+    {
+      title: "Cape Town, South Africa",
+      shortDescription:
+        "Explore the vibrant city, hike up Table Mountain for breathtaking views, and visit the stunning Cape Peninsula.",
+      longDescription:
+        "Nestled at the foot of the majestic Table Mountain, Cape Town is a vibrant and cosmopolitan city that effortlessly blends natural beauty with a rich cultural tapestry. This captivating destination offers a multitude of experiences, from pristine beaches and dramatic coastlines to world-class dining, eclectic markets, and a thriving arts scene. Immerse yourself in the city's fascinating history by exploring the iconic Robben Island, where Nelson Mandela was once imprisoned, or wander through the charming streets of the Bo-Kaap neighborhood with its colorful houses and Islamic heritage. Indulge in the renowned wine regions nearby, embark on breathtaking hikes, or simply soak up the breathtaking views from the top of Table Mountain. With its warm hospitality, diverse population, and a blend of old-world charm and contemporary flair, Cape Town is a city that captivates the hearts of all who visit.",
+      url: "/location/cape-town",
+      mainImg: capetown,
+      sideAttractions: [
+        {
+          title: "Robben Island",
+          img: capetownimg1,
+          description:
+            "Take a ferry from Cape Town's waterfront and visit Robben Island, a UNESCO World Heritage Site that once served as a political prison. Explore the prison complex and hear firsthand accounts from former inmates, including Nelson Mandela.",
+        },
+        {
+          title: "Boulders Beach",
+          img: capetownimg2,
+          description:
+            "Located in Simon's Town, Boulders Beach is famous for its resident African penguin colony. Get up close and personal with these charming creatures as you walk along the boardwalks and pristine sandy shores.",
+        },
+      ],
+    },
+    {
+      title: "Marrakech, Morocco",
+      shortDescription:
+        "Immerse yourself in the bustling souks, vibrant colors, and enchanting atmosphere of this historic city.",
+      longDescription:
+        "Welcome to Marrakech, a vibrant city that tantalizes the senses and captivates the soul. Located in the heart of Morocco, Marrakech is a mesmerizing fusion of ancient traditions and modern vitality. Immerse yourself in the bustling souks, where colorful textiles, aromatic spices, and intricate handicrafts create a sensory feast. Lose yourself in the labyrinthine streets of the medina, where hidden gems like the magnificent Bahia Palace and the serene Majorelle Garden await. As the sun sets, Djemaa el Fna Square comes alive with its vibrant energy, where snake charmers, street performers, and tantalizing food stalls create an atmosphere like no other. From the stunning architecture of the Koutoubia Mosque to the tranquility of the traditional hammams, Marrakech invites you to embark on an enchanting journey into its rich history, warm hospitality, and unforgettable charm.",
+      url: "/location/Marrakech",
+      mainImg: marrakech,
+      sideAttractions: [
+        {
+          title: "Bahia Palace",
+          img: marrakechimg1,
+          description:
+            "Immerse yourself in the opulent world of Moroccan architecture and design at the Bahia Palace. Marvel at the intricately carved ceilings, stunning courtyards, and colorful tiles that adorn this 19th-century palace, once the residence of the Grand Vizier.",
+        },
+        {
+          title: "Saadian Tombs",
+          img: marrakechimg2,
+          description:
+            "Unearth a hidden gem in Marrakech as you explore the Saadian Tombs. These ancient tombs date back to the 16th century but were rediscovered in 1917. Admire the exquisite craftsmanship of the mausoleums and the peaceful atmosphere of this historical burial site.",
+        },
+      ],
+    },
+    {
+      title: "Serengeti National Park, Tanzania",
+      shortDescription:
+        "Embark on a thrilling safari adventure in the vast plains of the Serengeti, home to abundant wildlife and the Big Five.",
+      longDescription:
+        "Welcome to the enchanting Serengeti National Park in Tanzania, where nature's majesty unfolds on an epic scale. Spanning vast savannahs, Serengeti is a sanctuary of captivating beauty and abundant wildlife. As the sun-kissed grasslands stretch as far as the eye can see, witness the breathtaking spectacle of the annual wildebeest migration, where millions of wildebeest, zebras, and other herbivores traverse the plains in search of fresh grazing. With lions prowling, cheetahs sprinting, and elephants grazing, the Serengeti offers an unparalleled opportunity to witness nature's raw power and delicate balance. Immerse yourself in this timeless landscape, where every sunrise paints the horizon with hues of gold and every sunset sets the sky ablaze. Serengeti National Park beckons adventurers, nature enthusiasts, and dreamers alike to experience the sheer wonder of Africa's untamed wilderness.",
+      url: "/location/serengeti",
+      mainImg: serengeti,
+      sideAttractions: [
+        {
+          title: "Ngorongoro Crater",
+          img: serengetiimg1,
+          description:
+            "Located adjacent to the Serengeti National Park, the Ngorongoro Crater is a stunning natural wonder and a UNESCO World Heritage Site. Embark on a guided tour to witness the diverse wildlife, including lions, elephants, and flamingos, within the caldera's unique ecosystem.",
+        },
+        {
+          title: "Olduvai Gorge",
+          img: serengetiimg2,
+          description:
+            "Take a fascinating detour to the Olduvai Gorge, often referred to as the 'Cradle of Mankind.'This archaeological site has yielded significant discoveries, providing insights into early human evolution. Explore the museum and learn about the rich history of human origins.",
         },
       ],
     },
